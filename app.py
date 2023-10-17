2e27bc53bcac87bd1ff1861b7eb0e9e78a96eee3
victor247k <victor.clash.3017@gmail.com>
2023-10-17 07:14:29 +0300

    started on the country function 60% done

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request
-import requests, json
+import requests, json, pprint
 from jinja2 import Environment
 
 app = Flask(__name__)
@@ -11,6 +11,11 @@ def format_number(value):
 
 app.jinja_env.filters['format_number'] = format_number
 
+def format_border(value):
+    return "".format(value)
+
+app.jinja_env.filters['format_border'] = format_border
+
 @app.route("/")
 def index():
     url = "https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital"
@@ -20,7 +25,7 @@ def index():
 
     for row in response:
         country = {
-            "name": row['name']['common'],
+            "name": row['name']['official'],
             "flags": {
                 "png": row['flags']['png'],
             },
@@ -33,3 +38,42 @@ def index():
     regions = ["africa", "america", "asia", "europe", "oceania"]
 
     return render_template("index.html", regions=regions, countries=countries)
+
+@app.route("/country/<country_name>")
+def country_page(country_name):
+
+    countries = []
+
+    url = "https://restcountries.com/v3.1/all?fields=name,flags,nativeName,region,subregion,capital,topLevelDomain,currencies,borders,population"
+    response = requests.get(url).json()
+
+    for index, value in enumerate(response):
+        country = {
+            "name": value['name']['official'],
+            "index": index
+        }
+        countries.append(country)
+
+    matching = [d for d in countries if d.get('name') == country_name]
+
+    if matching:
+        row = response[int(matching[0]['index'])]
+        country = {
+            "name": row['name']['official'],
+            "flags": {
+                "png": row['flags']['png'],
+            },
+            "population": row['population'],
+            "region": row['region'],
+            "capital": row['capital'][0] if row['capital'] else row['capital'],
+            "subregion": row['subregion'],
+            "nativeName": get_native_name(row['name']['nativeName']),
+
+        }
+
+        return render_template("country.html", country=country) #fill in values TODO!
+    else:
+        return redirect("/")
+    
+def get_native_name(data):
+    return "TODO!"
